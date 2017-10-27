73e11a4d808e34c1681f87af5676b6d06ad88d9f
陳美妤 <s3a432090@student.ncut.edu.tw>
2017-10-27 23:45:06 +0800

    練習 2：設定 Route 接收參數

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/',function(){
     return'welcome';
 });
-
+*/
 Route::get('/',function(){
     return view('welcome');
 });
-
+/*
 Route::get('/',function(){
     return redirect('welcome');
+});
+*/
+Route::get('hello/{name}',function($name){
+    return'Hello,'.$name;
+});
+Route::get('hello/{name?}',function($name='Everybody'){
+return'Hello,'.$name;
 });
