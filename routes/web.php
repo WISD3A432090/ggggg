d32e2cd76574f7bd5683f4a0c6ede85aa340318e
陳美妤 <s3a432090@student.ncut.edu.tw>
2017-10-27 23:52:04 +0800

    練習 5：設定 Route 前置

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,12 @@ Route::get('say/{name?}',['as'=>'hello.index',function($name= 'Everybody'){
 Route::get('aa/',function(){
     return'aa';
 });
+Route::get('dashboard',function(){
+    return'dashboard';
+});
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return'admin dashboard';
+    });
+});
+
