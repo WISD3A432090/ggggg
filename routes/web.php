d77d1c27a611d654c6e213b4d5ea88888714f004
陳美妤 <s3a432090@student.ncut.edu.tw>
2017-10-27 23:42:42 +0800

    練習 1：設定 Route 反應

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/',function(){
+    return'welcome';
+});
+
+Route::get('/',function(){
     return view('welcome');
+});
+
+Route::get('/',function(){
+    return redirect('welcome');
 });
